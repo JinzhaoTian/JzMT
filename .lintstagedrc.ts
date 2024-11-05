61a19962480c995ebdd4113b809062a0ade86c08
Jinzhao Tian <tianjinzhao@qq.com>
2024-11-05 20:41:30 +0800

    refactor: rename config files

--- .lintstagedrc.ts
+++ .lintstagedrc.ts
@@ -1,4 +1,4 @@
-import { Config } from 'lint-staged';
+import type { Config } from 'lint-staged';
 import path from 'path';
 
 const buildEslintCommand = (filenames: string[]): string =>
@@ -7,10 +7,7 @@ const buildEslintCommand = (filenames: string[]): string =>
         .join(' --file ')}`;
 
 const config: Config = {
-    // 为 js, jsx, ts, tsx 文件添加 eslint 命令
     '*.{js,jsx,ts,tsx}': [buildEslintCommand],
-
-    // 为其他类型的文件运行 prettier 格式化
     '**/*.{js,jsx,tsx,ts,less,md,json}': ['prettier --write']
 };
 
