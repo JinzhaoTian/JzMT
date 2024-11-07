7bac4ea5b072c8bfa8dea4e26b964b20db541c22
Jinzhao Tian <tianjinzhao@qq.com>
2024-11-08 00:52:42 +0800

    feat: change scroll area

--- src/app/edit/page.tsx
+++ src/app/edit/page.tsx
@@ -1,15 +1,18 @@
 'use client';
 
 import { AppEditor } from '@/components/app-editor';
+import { ScrollArea } from '@/components/ui/scroll-area';
 
 export default function Page() {
     return (
         <main className="flex flex-col flex-1 justify-center overflow-hidden">
-            <div className="flex justify-center w-full p-4 overflow-y-auto">
-                <div className="max-w-3xl">
-                    <AppEditor />
+            <ScrollArea className="h-[80rem]">
+                <div className="flex flex-col justify-center gap-2 p-4 pt-0">
+                    <div className="flex justify-center w-full p-4 max-w-3xl">
+                        <AppEditor />
+                    </div>
                 </div>
-            </div>
+            </ScrollArea>
         </main>
     );
 }
