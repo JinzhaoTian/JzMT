0df541fae0b20ae14b4ca5776bd1cfdc21fc5e8c
Jinzhao Tian <tianjinzhao@qq.com>
2024-11-14 00:05:17 +0800

    feat: test editor style

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,18 +1,13 @@
 import { AppEditor } from '@/components/app-editor';
-import { ScrollArea } from '@/components/ui/scroll-area';
 
 export default function Home() {
     return (
-        <main className="flex flex-col min-h-screen justify-center items-center">
-            <div className="flex flex-col flex-1 justify-center overflow-hidden border">
-                <ScrollArea className="h-[80rem] bg-gray-50">
-                    <div className="flex flex-col justify-center gap-2 p-4 pt-0">
-                        <div className="flex justify-center w-full p-4 max-w-3xl">
-                            <AppEditor />
-                        </div>
-                    </div>
-                </ScrollArea>
+        <main className="flex flex-col justify-center items-center h-full w-full">
+            <div className="h-16 w-full bg-gray-300 border border-red-300" />
+            <div className="h-[calc(100vh-6rem)] w-full overflow-hidden">
+                <AppEditor />
             </div>
+            <div className="h-8 w-full bg-gray-300 border border-red-300" />
         </main>
     );
 }
