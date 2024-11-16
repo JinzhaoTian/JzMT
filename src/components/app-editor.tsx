e4e018008d548d88d252833ae99654ea1bb4db0b
Jinzhao Tian <tianjinzhao@qq.com>
2024-11-16 13:15:32 +0800

    refactor: add tiptap extension

--- src/components/app-editor.tsx
+++ src/components/app-editor.tsx
@@ -1,21 +1,17 @@
 'use client';
 
 import { ScrollArea } from '@/components/ui/scroll-area';
-import Document from '@tiptap/extension-document';
+import { exampleText } from '@/contexts/example-text';
 import { EditorContent, useEditor } from '@tiptap/react';
 import StarterKit from '@tiptap/starter-kit';
 
-const text = `This is a radically reduced version of Tiptap. It has support for a document, with paragraphs and text. That’s it. It’s probably too much for real minimalists though.
-
-The paragraph extension is not really required, but you need at least one node. Sure, that node can be something different.`;
-
 export function AppEditor() {
     const editor = useEditor({
-        extensions: [Document, StarterKit],
-        content: text,
+        extensions: [StarterKit],
+        content: exampleText,
         editorProps: {
             attributes: {
-                class: 'max-w-[60rem] focus:outline-none'
+                class: 'max-w-[60rem] focus:outline-none font-editor'
             }
         }
     });
