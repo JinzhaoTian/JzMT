15f6b49d63a230c1c37b014b0d846be2eadf8889
Jinzhao Tian <tianjinzhao@qq.com>
2024-11-17 14:52:46 +0800

    feat: add lexical editor theme

--- src/components/editor/lexical-editor.tsx
+++ src/components/editor/lexical-editor.tsx
@@ -2,26 +2,27 @@
 
 import React from 'react';
 
-import { ScrollArea } from '@/components/ui/scroll-area';
-
 import { CodeNode } from '@lexical/code';
 import { LinkNode } from '@lexical/link';
 import { ListItemNode, ListNode } from '@lexical/list';
 import { $convertToMarkdownString, TRANSFORMERS } from '@lexical/markdown';
 import { LexicalComposer } from '@lexical/react/LexicalComposer';
-import { useLexicalComposerContext } from '@lexical/react/LexicalComposerContext';
-import { ContentEditable } from '@lexical/react/LexicalContentEditable';
 import { HistoryPlugin } from '@lexical/react/LexicalHistoryPlugin';
 import { MarkdownShortcutPlugin } from '@lexical/react/LexicalMarkdownShortcutPlugin';
 import { OnChangePlugin } from '@lexical/react/LexicalOnChangePlugin';
-import { RichTextPlugin } from '@lexical/react/LexicalRichTextPlugin';
 import { HeadingNode, QuoteNode } from '@lexical/rich-text';
 import { EditorState } from 'lexical';
 
+import CustomTextPlugin from './plugins/custom-text-plugin';
+import CustomTextTheme from './themes/custom-text-theme';
+
 const LexicalEditor: React.FC = () => {
     const initialConfig = {
-        namespace: 'MarkdownEditor',
-        onError: (error: Error) => console.error('Lexical Error:', error),
+        namespace: 'JzRM',
+        theme: CustomTextTheme,
+        onError: (error: Error) => {
+            console.error('Error:', error);
+        },
         nodes: [
             HeadingNode,
             QuoteNode,
@@ -42,44 +43,9 @@ const LexicalEditor: React.FC = () => {
         });
     };
 
-    // 焦点逻辑插件
-    const FocusPlugin = () => {
-        const [editor] = useLexicalComposerContext();
-
-        const handleClick = () => {
-            editor.focus(); // 聚焦到编辑器
-        };
-
-        return (
-            <ScrollArea
-                className="h-full w-full"
-                onClick={(e) => {
-                    e.stopPropagation(); // 阻止事件冒泡
-                    handleClick();
-                }}
-            >
-                <div className="flex justify-center border p-16 pt-2 bg-orange-100">
-                    <div className="relative">
-                        <RichTextPlugin
-                            contentEditable={
-                                <ContentEditable className="w-[60rem] outline-none font-editor border border-s-sky-700" />
-                            }
-                            placeholder={
-                                <div className="absolute w-full text-gray-400">
-                                    Start typing...
-                                </div>
-                            }
-                            ErrorBoundary={({ children }) => <>{children}</>}
-                        />
-                    </div>
-                </div>
-            </ScrollArea>
-        );
-    };
-
     return (
         <LexicalComposer initialConfig={initialConfig}>
-            <FocusPlugin />
+            <CustomTextPlugin />
             <HistoryPlugin />
             <MarkdownShortcutPlugin transformers={TRANSFORMERS} />
             <OnChangePlugin onChange={handleChange} />
---
+++ src/components/editor/plugins/custom-text-plugin.tsx
@@ -0,0 +1,37 @@
+import { ScrollArea } from '@/components/ui/scroll-area';
+
+import { useLexicalComposerContext } from '@lexical/react/LexicalComposerContext';
+import { ContentEditable } from '@lexical/react/LexicalContentEditable';
+import { RichTextPlugin } from '@lexical/react/LexicalRichTextPlugin';
+
+const CustomTextPlugin = () => {
+    const [editor] = useLexicalComposerContext();
+
+    return (
+        <ScrollArea
+            className="h-full w-full"
+            onClick={(e) => {
+                e.stopPropagation(); // 阻止事件冒泡
+                editor.focus();
+            }}
+        >
+            <div className="flex justify-center p-20 pt-2">
+                <div className="relative w-full">
+                    <RichTextPlugin
+                        contentEditable={
+                            <ContentEditable className="max-w-[60rem] outline-none font-editor" />
+                        }
+                        placeholder={
+                            <div className="absolute top-0 left-0 font-editor pointer-events-none text-gray-400">
+                                Start typing...
+                            </div>
+                        }
+                        ErrorBoundary={({ children }) => <>{children}</>}
+                    />
+                </div>
+            </div>
+        </ScrollArea>
+    );
+};
+
+export default CustomTextPlugin;
