067ccf8e86c093b1599d245a164bb23cd3d908bf
田金钊 <tianjinzhao@thape.com.cn>
2024-11-27 18:06:01 +0800

    feat: add test parser

--- src/components/editor/lexical-editor.tsx
+++ src/components/editor/lexical-editor.tsx
@@ -3,17 +3,14 @@
 import { CodeNode } from '@lexical/code';
 import { LinkNode } from '@lexical/link';
 import { ListItemNode, ListNode } from '@lexical/list';
-import { AutoFocusPlugin } from '@lexical/react/LexicalAutoFocusPlugin';
 import { LexicalComposer } from '@lexical/react/LexicalComposer';
-import { HistoryPlugin } from '@lexical/react/LexicalHistoryPlugin';
 
-import { TabIndentationPlugin } from '@lexical/react/LexicalTabIndentationPlugin';
 import { HeadingNode, QuoteNode } from '@lexical/rich-text';
 
 import CustomEditorTheme from './themes/custom-editor-theme';
 
 import { CustomHeadingNode } from './nodes/custom-heading-node';
-import CustomMarkdownPlugin from './plugins/custom-markdown-plugin';
+import CustomMarkdownParser from './plugins/custom-markdown-parser';
 import CustomOnChangePlugin from './plugins/custom-onChange-plugin';
 import CustomTextPlugin from './plugins/custom-text-plugin';
 
@@ -44,11 +41,8 @@ export default function LexicalEditor() {
     return (
         <LexicalComposer initialConfig={initialConfig}>
             <CustomTextPlugin />
-            <CustomMarkdownPlugin />
+            <CustomMarkdownParser />
             <CustomOnChangePlugin />
-            <TabIndentationPlugin />
-            <AutoFocusPlugin />
-            <HistoryPlugin />
         </LexicalComposer>
     );
 }
--- src/components/editor/plugins/custom-markdown-parser.tsx
+++ src/components/editor/plugins/custom-markdown-parser.tsx
@@ -1,24 +1,69 @@
 import { useEffect } from 'react';
 
+import {
+    $getNodeByKey,
+    $getSelection,
+    $isRangeSelection,
+    $isRootOrShadowRoot,
+    $isTextNode,
+    COMMAND_PRIORITY_LOW,
+    KEY_DOWN_COMMAND
+} from 'lexical';
+
 import { useLexicalComposerContext } from '@lexical/react/LexicalComposerContext';
-import { COMMAND_PRIORITY_LOW, KEY_DOWN_COMMAND } from 'lexical';
 
 export default function CustomMarkdownParser() {
     const [editor] = useLexicalComposerContext();
 
     useEffect(() => {
-        const unregisterCommand = editor.registerCommand(
+        return editor.registerCommand(
             KEY_DOWN_COMMAND,
-            () => {
-                editor.update(() => {});
+            (payload: string) => {
+                const selection = editor.getEditorState().read($getSelection);
+
+                if (!$isRangeSelection(selection) || !selection.isCollapsed())
+                    return false;
+
+                console.log('selection', selection);
+
+                const anchorKey = selection.anchor.key;
+                const anchorOffset = selection.anchor.offset;
+
+                const anchorNode = $getNodeByKey(anchorKey);
+
+                console.log('anchorNode', anchorNode);
+
+                if (!$isTextNode(anchorNode)) {
+                    return false;
+                }
+
+                const parentNode = anchorNode.getParent();
+
+                if (parentNode === null) {
+                    return false;
+                }
+
+                console.log('parentNode', parentNode);
+
+                const grandParentNode = parentNode.getParent();
+
+                if (
+                    !$isRootOrShadowRoot(grandParentNode) ||
+                    parentNode.getFirstChild() !== anchorNode
+                ) {
+                    return false;
+                }
+
+                console.log('grandParentNode', grandParentNode);
+
+                const textContent = anchorNode.getTextContent();
+
+                console.log('textContent', textContent);
+
                 return false;
             },
             COMMAND_PRIORITY_LOW
         );
-
-        return () => {
-            unregisterCommand();
-        };
     }, [editor]);
 
     return null;
